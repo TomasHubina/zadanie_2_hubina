71df815d10e8f70db470201d9f5c725c170e0735
Tomáš Hubina <hubina.tomas18@gmail.com>
2025-03-20 13:33:34 +0100

    prepojenie na inú stránku cez json

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@ function preparePortfolio(int $numberOfRows = 2, int $numberOfCols = 4): array {
 }
 
 function finishPortfolio() {
+    // Načítanie údajov zo súboru data.json
     $jsonData = file_get_contents(__DIR__ . '/data/data.json');
     $portfolioData = json_decode($jsonData, true);
 
@@ -20,15 +21,17 @@ function finishPortfolio() {
     foreach ($portfolio as $row => $col) {
         echo '<div class="row">';
         foreach ($col as $index) {
+            // Získanie údajov pre konkrétny index
             $fotoKey = 'portfolio_foto' . $index;
-            $foto = $portfolioData[$fotoKey]['foto'] ?? 'img/default.jpg';
+            $foto = $portfolioData[$fotoKey]['foto'] ?? 'img/default.jpg'; // Predvolený obrázok
             $alt = $portfolioData[$fotoKey]['alt'] ?? 'Web stránka ' . $index;
+            $url = $portfolioData[$fotoKey]['url'] ?? '#'; // Predvolený odkaz
 
-            echo '<div class="col-25 portfolio" id="portfolio-' . $index . '" style="background-image: url(\'' . $foto . '\');">
+            echo '<a href="' . $url . '" class="col-25 portfolio" id="portfolio-' . $index . '" style="background-image: url(\'' . $foto . '\');">
                 <div class="portfolio-content">
                     <p class="portfolio-text">' . $alt . '</p>
                 </div>
-            </div>';
+            </a>';
         }
         echo '</div>';
     }
